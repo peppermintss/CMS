fecfa19dc3ff9f0874911486f08093785ebb14fd
shalin-devkota <shalin.devkota01@gmail.com>
2023-03-19 15:10:34 +0545

    Updated user creation form

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib import messages
+from .forms import CreateUserForm
 
 def home_page(request):
     return render(request,"index.html")
 
 def register_page(request,methods=['GET','POST']):
+    form = CreateUserForm()
     if request.method == "POST":
-        username = request.POST.get('username')
-        password=request.POST.get('password')
-        user= User.objects.create_user(username,password=password)
-        return redirect('home-page')
-    else:
-        return render (request,'register.html')
+        form= CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home-page")
+    context = {'form':form}
+    return render (request,'register.html',context)
 
 def login_page(request):
     if request.method == "POST":
