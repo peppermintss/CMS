4d6956458ca1c894894b19aef50ec67eab8f61e6
shalin-devkota <shalin.devkota01@gmail.com>
2023-03-22 16:46:29 +0545

    Fixed static file

--- CMS/settings.py
+++ CMS/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_URL = 'static/'
 #Added manually
-STATICFILES_DIR = [
-    BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR /"static/"
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
