70bfc6abe11ab58593f4462dd0ccbea107fedbb0
shalin-devkota <shalin.devkota01@gmail.com>
2023-03-25 11:37:12 +0545

    hotfix

--- account/forms.py
+++ account/forms.py
@@ -10,8 +10,7 @@ class CreateUserForm(UserCreationForm):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         self.fields['password1'].required = False
         self.fields['password2'].required = False
-        # If one field gets autocompleted but not the other, our 'neither
-        # password or both password' validation will be triggered.
+        
         self.fields['password1'].widget.attrs['autocomplete'] = 'off'
         self.fields['password2'].widget.attrs['autocomplete'] = 'off'
 
