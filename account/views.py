2589d69c375ea27f22168ac91fa00eb288479eb4
shalin-devkota <shalin.devkota01@gmail.com>
2023-03-26 17:05:04 +0545

    Overhauled User creation UI

--- account/views.py
+++ account/views.py
@@ -3,6 +3,8 @@ from .forms import CreateUserForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 import random
+from .models import Account
+from django.urls import reverse
 #OO LOOK AT THIS IF ELSE LADDER OO ITS NOT GOOD PRACTISE OOO SO SCARY I CRY
 
 @login_required
@@ -37,36 +39,36 @@ def register_page(request,methods=['GET','POST']):
 
 #This function is very very ugly but it works. Maybe override the save() method instead and make it cleaner?
 
-def add_student(request):
 
-    faculty = request.headers['Referer']
-    faculty = faculty.replace("http://127.0.0.1:8000/", "").strip("/")
-    form = CreateUserForm()
+
+# Create your views here.
+def add_account(request,faculty,semester):
+    form = CreateUserForm
     if request.method == "POST":
+        group = "teacher" if faculty=="teacher" else "student"
         form= CreateUserForm(request.POST)
         if form.is_valid():
-            
-            group = "teacher" if faculty=="teacher" else "student"
-            student = form.save(commit=False)
+            new_account = form.save(commit=False)
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data ['last_name']
             phone_number = form.cleaned_data['phone_number']
-            semester = form.cleaned_data['semester']
-            student.username = f"{first_name + last_name + str(random.randint(1,100))}"
-            student.set_password(str(phone_number))
-            student.faculty = faculty
             
-            if faculty =="teacher":
-                semester = 0
-            student.semester = semester
-            student.save()
+            new_account.username = f"{first_name + last_name + str(random.randint(1,100))}"
+            new_account.set_password(str(phone_number))
+            new_account.faculty = faculty
+            new_account.semester = semester
+           
+            new_account.save()
             group= Group.objects.get(name=f"{group}")
-            student.groups.add(group)
+            new_account.groups.add(group)
             print(request.headers['Referer'])
             return redirect(request.headers['Referer'])
         else:
-            print (form.errors)
-    context = {'form':form,'faculty':faculty}
-    return render (request,'register.html',context)
+            print(form.errors)
 
-# Create your views here.
+    context = {
+        'form':form,
+        'faculty':faculty,
+        'semester':semester,
+        }
+    return render (request,'register.html',context)
--- course/views.py
+++ course/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
+from account.models import Account
 def course_detail_view(request,course):
     context= {
-        'course': course
+        'course': course,
+        'semesters': range(1,9)
     }
+    
     return render(request,"course_detail.html",context=context)
+
+
+def get_students_by_semester(request,course,semester):
+   
+    students = Account.objects.filter(faculty=course).filter(semester=semester)
+    context={
+        'faculty' : course,
+        'students':students,
+        'semester':semester
+    }
+    return render(request,"student-by-semester.html",context=context)
 
