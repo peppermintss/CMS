e358ab21529a3d123028b907d5fd086b23550ca6
shalin-devkota <shalin.devkota01@gmail.com>
2023-04-10 18:37:21 +0545

    limited teacher relationship to teacher group

--- faculty/models.py
+++ faculty/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from account.models import Account
+from django.contrib.auth.models import Group
 
 
 class Faculty(models.Model):
@@ -12,7 +13,11 @@ class Faculty(models.Model):
 class Subject(models.Model):
     name = models.CharField(max_length=30, unique=True)
     faculty = models.ForeignKey(Faculty, to_field="name", on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    teacher = models.ForeignKey(
+        Account,
+        limit_choices_to={"groups__name": "teacher"},
+        on_delete=models.DO_NOTHING,
+    )
     semester = models.IntegerField()
 
     def __str__(self):
